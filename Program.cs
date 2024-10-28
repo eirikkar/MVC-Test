606b747b03f5d297d528ad070f2e0211f0b61328
eirikkar <eirikkar@proton.me>
2024-10-28 12:38:59 +0100

    added functions and objects to main function

--- Program.cs
+++ Program.cs
@@ -2,5 +2,14 @@
 
 class Program
 {
-    static void Main(string[] args) { }
+    static void Main(string[] args)
+    {
+        Model model = new Model("John Doe", 40);
+        View view = new();
+        Controller controller = new Controller(model, view);
+        controller.ShowModel();
+        controller.UpdateName();
+        controller.UpdateAge();
+        controller.ShowModel();
+    }
 }
